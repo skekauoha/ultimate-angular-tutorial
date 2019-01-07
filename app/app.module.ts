03eddfa8d08b6818224322b09a42eabab400aa93
Spencer Kekauoha <skekauoha@saltadminsMBP4.hsd1.ut.comcast.net>
2019-01-06 18:02:44 -0700

    add routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
+
+import { HomeComponent } from './home.component';
+import { NotFoundComponent } from './not-found.component';
 
 import { AppComponent } from './app.component';
 
+const routes: Routes = [
+  { path: '', redirectTo: 'passengers', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
+];
+
 @NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent
+  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    RouterModule.forRoot(routes, { useHash: true }),
+    PassengerDashboardModule
   ],
   bootstrap: [
-    AppComponent
-  ],
-  declarations: [
     AppComponent
   ]
 })
---
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -0,0 +1,115 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+import { Passenger } from '../../models/passenger.interface';
+import { Baggage } from '../../models/baggage.interface';
+
+@Component({
+  selector: 'passenger-form',
+  styleUrls: ['passenger-form.component.scss'],
+  template: `
+  <!--setting a ref of form to ngForm which exports ngForm (built in ng directive) to keep track of state changes and validations of form -->
+  <!-- novalidate not validate, use ng built in validation -->
+  <form (ngSubmit)="handleSubmit(form.value, form.valid)" #form="ngForm" novalidate>
+      {{ detail | json }}
+
+      <div>
+        Passenger name:
+        <input
+          type="text"
+          name="fullname"
+          required
+          #fullname="ngModel"
+          [ngModel]="detail?.fullname">
+
+          <div *ngIf="fullname.errors?.required && fullname.dirty" class="error">
+            Passenger name is required
+          </div>
+      </div>
+
+      <div>
+        Passenger ID:
+        <input
+          type="number"
+          name="id"
+          required
+          #id="ngModel"
+          [ngModel]="detail?.id">
+
+          <div *ngIf="id.errors?.required && id.dirty" class="error">
+            Passenger ID is required
+          </div>
+      </div>
+
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            name="checkedIn"
+            [ngModel]="detail?.checkedIn"
+            (ngModelChange)="toggleCheckIn($event)">
+        </label>
+      </div>
+
+      <div *ngIf="form.value.checkedIn">
+        Check in date:
+        <input
+          type="number"
+          name="checkInDate"
+          [ngModel]="detail.checkInDate">
+      </div>
+
+      <div>
+        Luggage:
+        <select
+          name="baggage"
+          [ngModel]="detail?.baggage">
+          <option
+            *ngFor="let item of baggage"
+            [value]="item.key"
+            [selected]="item.key === detail?.baggage">
+             {{ item.value }}
+          </option>
+        </select>
+      </div>
+
+      <button type="submit" [disabled]="form.invalid">
+        Update Passenger
+      </button>
+    </form>
+  `
+})
+
+export class PassengerFormComponent {
+  @Input() detail: Passenger;
+
+  @Output() update: EventEmitter<Passenger> = new EventEmitter<Passenger>();
+
+  baggage: Baggage[] = [{
+    key: 'none',
+    value: 'No baggage'
+  },
+  {
+    key: 'hand-only',
+    value: 'Hand baggage'
+  },
+  {
+    key: 'hold-only',
+    value: 'Hold baggage'
+  },
+  {
+    key: 'hand-hold',
+    value: 'Hand and hold baggage'
+  }]
+
+  toggleCheckIn(checkedIn: boolean) {
+    if (checkedIn) {
+      this.detail.checkInDate = Date.now();
+    }
+  }
+
+  handleSubmit(passenger: Passenger, isValid: boolean) {
+    if (isValid) {
+      this.update.emit(passenger);
+    }
+  }
+}
